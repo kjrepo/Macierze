8a999a1ea23d81adbd03c32ee9bdcb4252278a92
Sevitte <sevitte@o2.pl>
2018-12-02 17:10:54 +0100

    fix

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,95 +5,39 @@ import java.math.BigInteger;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		/// Generowanie double
+		MyMatrix<Float> testF = new MyMatrix<>(10, 10, Float.class);
+		MyMatrix<Double> testD = new MyMatrix<>(10, 10, Double.class);
+		MyMatrix<TC> testTC = new MyMatrix<>(30, 30, TC.class);
+		
+		testF.pop();
+		testF.popVector();
+		testF.GaussNoChoice();
+		testF.GaussHalfChoice();
+		testF.GaussFullChoice();
+		testF.chceck();
+		testF.chceckf();
+		testF.chceckh();
 
-
-		System.out.println("Double");
-
-		System.out.println("");
+		testD.pop();
+		testD.popVector();
+		testD.GaussNoChoice();
+		testD.GaussHalfChoice();
+		testD.GaussFullChoice();
+		testD.chceck();
+		testD.chceckf();
+		testD.chceckh();
 		
-		float a = 11; 
-		float b = 2;
-		double c = 3;
+		testTC.pop();
+		testTC.popVector();
+		testTC.GaussNoChoice();
+		testTC.GaussHalfChoice();
+		testTC.GaussFullChoice();
+		testTC.chceck();
+		testTC.chceckf();
+		testTC.chceckh();
 		
-		
-//		MyMatrix<Double> DoubleTest = new MyMatrix<>(5, 5, Double.class);
-//		DoubleTest.pop();
-//		DoubleTest.popVector();
-//		DoubleTest.GaussFullChoice(DoubleTest.tab, DoubleTest.vector, 5);
-		
-		
-		MyMatrix<Float> test = new MyMatrix<>(5, 5, Float.class);
-		
-		
-		test.pop();
-		test.popVector();
-		test.multiplyMatrix();
-		System.out.println("XD");
-		test.zeroVector();
-		test.GaussNoChoice();
-		test.test();
-		test.chceck();
-		
-		
-//		System.out.println(test.add(Float.valueOf(2),Float.valueOf(2)));
-//		
-//		System.out.println("\n\n//////////////////////////////");
-//		
-//		MyMatrix<TC> testTC = new MyMatrix<>(6, 6, TC.class);
-//		testTC.pop();
-//		testTC.test();
-////		testTC.GaussFullChoice(testTC.tab, testTC.vector, 6);
-//		
-//		TC one = new TC(BigInteger.valueOf(1),BigInteger.valueOf(2));
-//		TC two = new TC(BigInteger.valueOf(1),BigInteger.valueOf(4));
-//		
-//		System.out.println(testTC.add(one, two).toString());
-//		System.out.println(testTC.sub(one, two).toString());
-//		System.out.println(testTC.mul(one, two).toString());
-//		System.out.println(testTC.div(one, two).toString());
-
-//		MyMatrix<Double> doubleTest = new MyMatrix<Double>(6,6);
-//		doubleTest.populate(rand ->  Double.valueOf(rand.nextInt(131072) - Math.pow(2, 16))/Math.pow(2, 16));
-//		doubleTest.test();
-//		System.out.println("!!!!!!!");
-//		doubleTest.populateL(rand ->  Double.valueOf(rand.nextInt(131072) - Math.pow(2, 16))/Math.pow(2, 16));
-//		doubleTest.testL();
-//		
-//		
-//		//Generowanie float
-//		System.out.println("\n\n\n");
-//		System.out.println("Floaty");
-//		System.out.println("");
-//
-//		
-//		MyMatrix<Float> floatTest = new MyMatrix<Float>(6,6);
-//		floatTest.populateL(rand -> ((float) rand.nextInt(zakres*2) - zakres)/zakres);
-//		floatTest.testL();
-//		floatTest.GaussEliminationFloat();
-//		System.out.println("???????????????????");
-//		floatTest.testL();
-//		
-//		
-//		System.out.println("\n\n\n");
-//		System.out.println("Stworzona liczba");
-//		System.out.println("");
-//		
-//		TC liczba = new TC();
-//		liczba.nextTC();
-//		System.out.println("Licznik "+liczba.top);
-//		System.out.println("Mianownik "+liczba.bot);
-//		System.out.println("W zapisie dziesietnym "+liczba.top.divide(liczba.bot));
-//		
-//		// Generowanie TC
-//		
-//		MyMatrix<TC> TCTest = new MyMatrix<TC>(6,6);
-//		TCTest.populate(rand -> rand.nextTC());
-//		TCTest.testG(t -> t.top.toString()+"/"+t.bot.toString());
-//		
-		
+	
 	}
 
 }
--- src/app/MyMatrix.java
+++ src/app/MyMatrix.java
@@ -11,10 +11,21 @@ public class MyMatrix<T> {
 	//// nowy poczatek
 	public T[][] tab;
 	public T[][] vector; 
-	//public T[][] nullVector;
 	public T[][] wynik;
 	public T[][] temp;
 	public T[][] tempV;
+
+	public T[][] wynikH;
+	public T[][] tempH;
+	public T[][] tempVH;
+	public T[][] vectorH; 
+
+	
+	public T[][] wynikF;
+	public T[][] tempF;
+	public T[][] tempVF;
+	public T[][] vectorF; 
+
 	
 	private final Class<T> type;
 	final int zakres= 65536;
@@ -22,20 +33,41 @@ public class MyMatrix<T> {
 	
 	/////
 
+	@SuppressWarnings("unchecked")
 	public void chceck() {
-		for(int i = 0; i < width; i++) {
-			for(int j = 0; j < width; j++) {
-				temp[j][i] = mul(temp[j][i], wynik[j][0]); 
-			}
+		T wyniczek = (T) new Object();
+		for(int i = 0 ; i < width ; i++) {
+			if (i==0)
+				wyniczek = mul(temp[0][0],vector[0][0]);
+			else
+				wyniczek = add(wyniczek,mul(temp[0][i],vector[i][0]));
 		}
-		
-		for(int i = 0; i < width; i++) {
-			for(int j = 0; j < width-1; j++) {
-				tempV[j][0] = add(temp[j][i], temp[j+1][i]);
-			}
+		System.out.println(abs(sub(tempV[0][0],wyniczek)));
+	}
+
+	@SuppressWarnings("unchecked")
+	public void chceckf() {
+		T wyniczekf = (T) new Object();
+		for(int i = 0 ; i < width ; i++) {
+			if (i==0)
+				wyniczekf = mul(tempF[0][0],vectorF[0][0]);
+			else
+				wyniczekf = add(wyniczekf,mul(tempF[0][i],vectorF[i][0]));
 		}
+		System.out.println(abs(sub(tempVH[0][0],wyniczekf)));
 	}
 	
+	@SuppressWarnings("unchecked")
+	public void chceckh() {
+		T wyniczekh = (T) new Object();
+		for(int i = 0 ; i < width ; i++) {
+			if (i==0)
+				wyniczekh = mul(tempH[0][0],vectorH[0][0]);
+			else
+				wyniczekh = add(wyniczekh,mul(tempH[0][i],vectorH[i][0]));
+		}
+		System.out.println(abs(sub(tempVH[0][0],wyniczekh)));
+	}
 	
 	@SuppressWarnings("unchecked")
 	public MyMatrix(int height, int width, Class<T> type) {	
@@ -44,6 +76,19 @@ public class MyMatrix<T> {
 		tempV = (T[][]) new Object[width][1];
 		vector = (T[][]) new Object[width][1];
 		wynik = (T[][]) new Object[width][height+1];
+		
+		tempVH = (T[][]) new Object[width][1];
+		tempH = (T[][]) new Object[width][height];
+		wynikH = (T[][]) new Object[width][height+1];
+		vectorH = (T[][]) new Object[width][1];
+
+		
+		tempVF = (T[][]) new Object[width][1];
+		tempF = (T[][]) new Object[width][height];
+		wynikF = (T[][]) new Object[width][height+1];
+		vectorF = (T[][]) new Object[width][1];
+
+		
 		this.height = height;
 		this.width = width;
 		
@@ -70,7 +115,15 @@ public class MyMatrix<T> {
 				for(int j = 0; j < height; j++) {
 					Object o = ((float) rand.nextInt(zakres*2) - zakres)/zakres;
 					tab[i][j] = (T) o;
+					wynik[i][j] = (T) o;
 					temp[i][j] = (T) o;
+					
+					wynikH[i][j] = (T) o;
+					tempH[i][j] = (T) o;
+					
+					wynikF[i][j] = (T) o;
+					tempF[i][j] = (T) o;
+
 				}
 			
 		}else if(type == Double.class){
@@ -78,19 +131,92 @@ public class MyMatrix<T> {
 				for(int j = 0; j < height; j++) {
 					Object o = ((double) rand.nextInt(zakres*2) - zakres)/zakres;
 					tab[i][j] = (T) o;
+					wynik[i][j] = (T) o;
 					temp[i][j] = (T) o;
+					
+					wynikH[i][j] = (T) o;
+					tempH[i][j] = (T) o;
+					
+					wynikF[i][j] = (T) o;
+					tempF[i][j] = (T) o;
+
 				}
 		}else if(type == TC.class){
 			for(int i = 0; i<width; i++)
 				for(int j = 0; j < height; j++) {
 					Object o = rand.nextTC();
 					tab[i][j] = (T) o;
+					wynik[i][j] = (T) o;
 					temp[i][j] = (T) o;
+
+					wynikH[i][j] = (T) o;
+					tempH[i][j] = (T) o;
+					
+					wynikF[i][j] = (T) o;
+					tempF[i][j] = (T) o;
+					
 				}
 		}
 		
 		
 	}
+	
+	public void popTest() {
+				double a = -2;
+				temp[0][0] = (T) (Object) (float) 4.0;
+				temp[0][1] = (T) (Object) (float)a;
+				temp[0][2] = (T) (Object) (float)4.0;
+				temp[0][3] = (T) (Object)(float) (-2.0);
+				tempV[0][0] = (T) (Object) (float)8.0;
+	
+				temp[1][0] = (T) (Object)(float) 3.0;
+				temp[1][1] = (T) (Object) (float)1.0;
+				temp[1][2] = (T) (Object) (float)4.0;
+				temp[1][3] = (T) (Object) (float)2.0;
+				tempV[1][0] = (T) (Object) (float)7.0;
+				
+				temp[2][0] = (T) (Object)(float) 2.0;
+				temp[2][1] = (T) (Object)(float) 4.0;
+				temp[2][2] = (T) (Object)(float) 2.0;
+				temp[2][3] = (T) (Object)(float) 1.0;
+				tempV[2][0] = (T) (Object) (float)10.0;
+				
+				temp[3][0] = (T) (Object)(float) 2.0;
+				temp[3][1] = (T) (Object)(float) (-2.0);
+				temp[3][2] = (T) (Object)(float) 4.0;
+				temp[3][3] = (T) (Object)(float) 2.0;
+				tempV[3][0] = (T) (Object)(float) 2.0;
+				
+				
+				
+				///
+				
+				wynik[0][0] = (T) (Object) (float) 4.0;
+				wynik[0][1] = (T) (Object) (float)a;
+				wynik[0][2] = (T) (Object) (float)4.0;
+				wynik[0][3] = (T) (Object)(float) (-2.0);
+				wynik[0][4] = (T) (Object) (float)8.0;
+	
+				wynik[1][0] = (T) (Object)(float) 3.0;
+				wynik[1][1] = (T) (Object) (float)1.0;
+				wynik[1][2] = (T) (Object) (float)4.0;
+				wynik[1][3] = (T) (Object) (float)2.0;
+				wynik[1][4] = (T) (Object) (float)7.0;
+				
+				wynik[2][0] = (T) (Object)(float) 2.0;
+				wynik[2][1] = (T) (Object)(float) 4.0;
+				wynik[2][2] = (T) (Object)(float) 2.0;
+				wynik[2][3] = (T) (Object)(float) 1.0;
+				wynik[2][4] = (T) (Object) (float)10.0;
+				
+				wynik[3][0] = (T) (Object)(float) 2.0;
+				wynik[3][1] = (T) (Object)(float) (-2.0);
+				wynik[3][2] = (T) (Object)(float) 4.0;
+				wynik[3][3] = (T) (Object)(float) 2.0;
+				wynik[3][4] = (T) (Object)(float) 2.0;
+	}
+	
+	
 	
 	public void popVector() {
 		
@@ -101,7 +227,17 @@ public class MyMatrix<T> {
 				
 					Object o = ((float) rand.nextInt(zakres*2) - zakres)/zakres;
 					vector[i][0] = (T) o;
+					wynik[i][width] = (T) o;
+					wynikF[i][width] = (T) o;
+
+					wynikH[i][width] = (T) o;
+
 					tempV[i][0] = (T) o;
+
+					tempVH[i][0] = (T) o;
+					
+					tempVF[i][0] = (T) o;
+
 
 			}
 			
@@ -110,7 +246,14 @@ public class MyMatrix<T> {
 				 {
 					Object o = ((double) rand.nextInt(zakres*2) - zakres)/zakres;
 					vector[i][0] = (T) o;
+					wynik[i][width] = (T) o;
+					wynikF[i][width] = (T) o;
+
+					wynikH[i][width] = (T) o;
 					tempV[i][0] = (T) o;
+					tempVH[i][0] = (T) o;
+					
+					tempVF[i][0] = (T) o;
 
 				}
 		}else if(type == TC.class){
@@ -118,7 +261,14 @@ public class MyMatrix<T> {
 				 {
 					Object o = rand.nextTC();
 					vector[i][0] = (T) o;
+					wynik[i][width] = (T) o;
+					wynikF[i][width] = (T) o;
+
+					wynikH[i][width] = (T) o;
 					tempV[i][0] = (T) o;
+					tempVH[i][0] = (T) o;
+					
+					tempVF[i][0] = (T) o;
 				}
 		}
 		
@@ -496,61 +646,7 @@ public class MyMatrix<T> {
 		}
 	}
 	
-//	@SuppressWarnings("unchecked")
-//	public T[][] multiplyMatrixTEST()
-//	{
-//		@SuppressWarnings("unchecked")
-//		T solution[][] = (T[][]) new Object[width][1];
-//		for(int i = 0; i < width; i++) {
-//			solution[i][0] = (T) Float.valueOf(0);
-//		}
-//		
-//		
-//		for(int counter = 0; counter < width; counter++) {
-//            for (int counter2 = 0; counter2 < 1; counter2++) {
-//                for (int counter3 = 0; counter3 < width; counter3++) {
-//                	solution[counter][counter2] = add(solution[counter][counter2] ,(mul(tab[counter][counter3] , vector[counter3][counter2])));
-//                }
-//            }
-//       }
-//
-//		return solution;
-//	}
-	
-	
-//	public void GaussNoChoiceTEST() <- To dzia³a (w miare)
-//	{
-//		int counter, counter2, counter3;
-//		T multiplier;
-//		T sum;
-//		double epsilon = Math.pow(10, -13);
-//		boolean isSolution = false;
-//		for(counter = 0 ; counter <= width - 2 ; counter++)
-//		{
-//			for(counter2 = counter + 1 ; counter2 <= width - 1 ; counter2++)
-//			{
-//				if(cmp(abs(tab[counter][counter2]) , epsilon) == -1)
-//					break;
-//				multiplier = neg(div((tab[counter2][counter]),(tab[counter][counter2])));
-//				for(counter3 = counter + 1 ; counter3 <= width; counter3++)
-//				{
-//					tab[counter2][counter3] = add(tab[counter2][counter3] , (mul(multiplier , tab[counter][counter3])));
-//				}
-//			}
-//		}
-//		for(counter = width - 1 ; counter > 0; counter--)
-//		{
-//			sum = tab[counter][width];
-//			for(counter2 = width - 1 ; counter2 > counter +1  ; counter2--)
-//			{
-//				sum = sub(sum , mul(tab[counter][counter2], vector[counter2][0]));
-//			}
-//			if(cmp(abs(tab[counter][counter]) , epsilon) == -1)
-//				break;
-//			vector[counter][0] = div(sum,tab[counter][counter]);
-//			isSolution = true;
-//		}
-//	}
+
 
 	public void GaussNoChoice()
 	{
@@ -561,12 +657,12 @@ public class MyMatrix<T> {
 		boolean isSolution = false;
 		for(counter = 0 ; counter <= width - 2 ; counter++)
 		{
-			for(counter2 = counter + 1 ; counter2 <= width - 1 ; counter2++)
+			for(counter2 = counter + 1 ; counter2 <= width -1 ; counter2++)
 			{
-				if(cmp(abs(wynik[counter][counter2]) , epsilon) == -1)
+				if(cmp(abs(wynik[counter][counter]) , epsilon) == -1)
 					break;
-				multiplier = neg(div((wynik[counter2][counter]),(wynik[counter][counter2])));
-				for(counter3 = counter + 1 ; counter3 <= width; counter3++)
+				multiplier = neg(div((wynik[counter2][counter]),(wynik[counter][counter])));
+				for(counter3 = counter + 1 ; counter3 <= width ; counter3++)
 				{
 					wynik[counter2][counter3] = add(wynik[counter2][counter3] , (mul(multiplier , wynik[counter][counter3])));
 				}
@@ -586,7 +682,7 @@ public class MyMatrix<T> {
 		}
 	}
 	
-	public void GaussHalfChoice(T matrix[][],T vector[][], int lenght)
+	public void GaussHalfChoice()
 	{
 		int counter, counter2, counter3;
 		T multiplier;
@@ -594,37 +690,36 @@ public class MyMatrix<T> {
 		double epsilon = Math.pow(10, -13);
 		boolean isSolution = false;
 		int biggestInRow;
-		for(counter = 0 ; counter <= lenght - 2 ; counter++)
+		for(counter = 0 ; counter <= width - 2 ; counter++)
 		{
 
-			for(counter2 = counter + 1 ; counter2 <= lenght - 1 ; counter2++)
+			for(counter2 = counter + 1 ; counter2 <= width - 1 ; counter2++)
 			{
-				if(cmp(abs(matrix[counter][counter2]) , epsilon) == -1)
+				if(cmp(abs(wynikH[counter][counter]) , epsilon) == -1)
 					break;
 				biggestInRow=biggestInRow(counter);	
 				swapRowes(biggestInRow, counter2);
-				multiplier = neg(div((matrix[counter2][counter]),(matrix[counter][counter2])));
-				for(counter3 = counter + 1 ; counter3 <= lenght; counter3++)
+				multiplier = neg(div((wynikH[counter2][counter]),(wynikH[counter][counter])));
+				for(counter3 = counter + 1 ; counter3 <= width ; counter3++)
 				{
-					matrix[counter2][counter3] = add(matrix[counter2][counter3] , (mul(multiplier , matrix[counter][counter3])));
+					wynikH[counter2][counter3] = add(wynikH[counter2][counter3] , (mul(multiplier , wynikH[counter][counter3])));
 				}
 			}
 		}
-		for(counter = lenght - 1 ; counter >= 0; counter--)
+		for(counter = width - 1 ; counter >= 0; counter--)
 		{
-			sum = matrix[counter][lenght+1];
-			for(counter2 = lenght - 1 ; counter2 >= counter + 1 ; counter--)
+			sum = wynikH[counter][width];
+			for(counter2 = width - 1 ; counter2 >= counter + 1 ; counter2--)
 			{
-				sum = sub(sum , mul(matrix[counter][counter2], vector[counter2][1]));
+				sum = sub(sum , mul(wynikH[counter][counter2], vectorH[counter2][0]));
 			}
-			if(cmp(abs(matrix[counter][counter]) , epsilon) == -1)
+			if(cmp(abs(wynikH[counter][counter]) , epsilon) == -1)
 				break;
-			vector[counter][1] = div(sum,matrix[counter][counter]);
+			vectorH[counter][0] = div(sum,wynikH[counter][counter]);
 			isSolution = true;
 		}
 	}
-	
-	public void GaussFullChoice(T matrix[][],T vector[][], int lenght)
+	public void GaussFullChoice()
 	{
 		int counter, counter2, counter3;
 		T multiplier;
@@ -632,36 +727,35 @@ public class MyMatrix<T> {
 		double epsilon = Math.pow(10, -13);
 		boolean isSolution = false;
 		int biggestInRowColumn[];
-		for(counter = 0 ; counter <= lenght - 2 ; counter++)
+		for(counter = 0 ; counter <= width - 2 ; counter++)
 		{
 
-			for(counter2 = counter + 1 ; counter2 <= lenght - 1 ; counter2++)
+			for(counter2 = counter + 1 ; counter2 <= width - 1 ; counter2++)
 			{
-				if(cmp(abs(matrix[counter][counter2]) , epsilon) == -1)
+				if(cmp(abs(wynikF[counter][counter]) , epsilon) == -1)
 					break;
 				biggestInRowColumn=biggestInRowColumn(counter);	
 				swapRowes(biggestInRowColumn[0], counter2);
 				swapColumns(biggestInRowColumn[1], counter);
-				multiplier = neg(div((matrix[counter2][counter]),(matrix[counter][counter2])));
-				for(counter3 = counter + 1 ; counter3 <= lenght; counter3++)
+				multiplier = neg(div((wynikF[counter2][counter]),(wynikF[counter][counter])));
+				for(counter3 = counter + 1 ; counter3 <= width ; counter3++)
 				{
-					matrix[counter2][counter3] = add(matrix[counter2][counter3] , (mul(multiplier , matrix[counter][counter3])));
+					wynikF[counter2][counter3] = add(wynikF[counter2][counter3] , (mul(multiplier , wynikF[counter][counter3])));
 				}
 			}
 		}
-		for(counter = lenght - 1 ; counter >= 0; counter--)
+		for(counter = width - 1 ; counter >= 0; counter--)
 		{
-			sum = matrix[counter][lenght+1];
-			for(counter2 = lenght - 1 ; counter2 >= counter + 1 ; counter--)
+			sum = wynikF[counter][width];
+			for(counter2 = width - 1 ; counter2 >= counter + 1 ; counter2--)
 			{
-				sum = sub(sum , mul(matrix[counter][counter2], vector[counter2][1]));
+				sum = sub(sum , mul(wynikF[counter][counter2], vectorF[counter2][0]));
 			}
-			if(cmp(abs(matrix[counter][counter]) , epsilon) == -1)
+			if(cmp(abs(wynikF[counter][counter]) , epsilon) == -1)
 				break;
-			vector[counter][1] = div(sum,matrix[counter][counter]);
+			vectorF[counter][0] = div(sum,wynikF[counter][counter]);
 			isSolution = true;
 		}
 	}
-	
 }
 	
